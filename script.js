70e662223c9a67e1783d0d3c57790d5fdfecff58
Durga-velu <durgadevi134babu@gmail.com>
2025-03-24 23:31:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -24,7 +24,8 @@ SportsCar.prototype.getTopSpeed=function(){
 
 
 const car = new SportsCar("Ferrari", "Testarossa", 200)
-
+console.log(car.getMakeModel())
+console.log(car.getTopSpeed())
 // Do not change the code below
 window.Car = Car;
 window.SportsCar = SportsCar;
