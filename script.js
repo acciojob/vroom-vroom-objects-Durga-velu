1432051767160d34a4763cb970e43f4c31fae415
Durga-velu <durgadevi134babu@gmail.com>
2025-03-24 16:00:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
 
-function SportsCar(make, model, topSpeed) {}
+	constructor(make,model){
+		this.make=make;
+		this.model=model
+	}
+}
+
+function SportsCar(make, model, topSpeed) {
+
+	constructor(make,model,topSpeed){
+		super();
+		this.make=make;
+		this.model=model;
+		this.topSpeed=topSpeed;
+	}
+	
+}
 
 // Do not change the code below
 window.Car = Car;
