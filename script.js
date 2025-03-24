f7dbd2b897ade0729065fbac5d50901b42e782bb
Durga-velu <durgadevi134babu@gmail.com>
2025-03-24 23:21:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 // Complete the js code
 function Car(make, model) {
-
-	constructor(make,model){
 		this.make=make;
 		this.model=model
-	}
+	
 }
 	Car.prototype.getMakeModel(){
 		return `${this.make} ${this.model} `
@@ -26,13 +24,7 @@ SportsCar.prototype.getTopSpeed=function () {
         return.this.topSpeed	
 }
 
-const car = new SportsCar("Ferrari", "Testarossa", 200);
 
-
-console.log(car.getMakeModel());
-
-
-console.log(car.getTopSpeed())
 
 
 // Do not change the code below
